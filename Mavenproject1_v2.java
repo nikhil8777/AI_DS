398124027f34187784d7ce2da86671bdd1c61470
nikhil8777 <nikhil@loyalistcollege.com>
2022-06-28 21:03:46 -0400

    first commit

---
+++ Mavenproject1_v2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author nikhil
+ */
+public class Mavenproject1_v2 {
+
+    public static void main(String[] args) {
+        
+      int num1 = 15, num2 = 25, sum;
+      int num3 = 15, num4 = 25, sub;
+      int num5 = 15, num6 = 25, mul;
+      sum = num1 + num2;
+      sub = num3 - num4;
+      mul = num5 * num6;
+
+      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("sub of these numbers: "+sub);
+      System.out.println("Mul of these numbers: "+mul);
+   }
+}
+
